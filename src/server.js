382676295cbf0bed4d230feddfd7cd9095b671a4
Gabrield7 <gabrieldantas503@gmail.com>
2026-09-04 14:41:02 -0300

    fix: app.listen no server.js

--- src/server.js
+++ src/server.js
@@ -25,6 +25,6 @@ await seedGestor();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
